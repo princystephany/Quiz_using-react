67de04e122fe15086ba76fab7392928a2d27cbcf
princystephany <stephanyben6@gmail.com>
2023-04-26 23:49:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+   import './App.css';
+
+import Questions from './component/Questions.js'
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const[questionIndex, setQuestionIndex]=useState(0)
+
+  const present_question = Questions[questionIndex];
+ const get_choice =(index) =>
+{
+if(present_question.answer===index){
+  alert("success")
+}
+}  
+    return (
+    <div className="quiz_box">
+       <div className="quiz_question">
+{present_question.question}
+<div className="options">Obtions below
+</div>
+
+<ul className="quiz_ul">
+
+  {present_question.options.map
+  (
+(option,i)=> {
+
+  return<li className='quiz_li' onClick={
+() => get_choice(i)
+    
+  }>{option}</li> 
+}
+  )
+  }
+</ul>
+       </div>
     </div>
   );
 }
---
+++ src/component/Questions.js
@@ -0,0 +1,35 @@
+export default[
+    {
+question:"Who is the President of India?",
+options:["A.Modi", "B.Draupadi Murmu", "C.Nirmala Sitaraman", "D. Abdulkalam"],
+answer:1,
+
+    },
+     {
+        question:"2St",
+        options:["A", "B", "C", "D"],
+        answer:0,
+        
+            },
+ {
+                question:"3St",
+                options:["A", "B", "C", "D"],
+                answer:0,
+                
+                    },
+                    {
+                        question:"4St",
+                        options:["A", "B", "C", "D"],
+                        answer:0,
+                        
+                            },
+                            {
+                                question:"5St",
+                                options:["A", "B", "C", "D"],
+                                answer:0,
+                                
+                                    },                    
+
+
+
+]
