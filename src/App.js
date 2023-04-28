df5328f4ba36463b61ce9585a5f5b6eb77b638cc
princystephany <stephanyben6@gmail.com>
2023-04-28 01:09:55 -0700

    functionality complete

--- src/App.js
+++ src/App.js
@@ -1,59 +1,77 @@
-   import './App.css';
+      import './App.css';
 
-import Questions from './component/Questions.js'
-import {useState} from "react";
+   import Questions from './component/Questions.js'
+   import {useState} from "react";
 
-function App() {
+   function App() {
 
-  const[questionIndex, setQuestionIndex]=useState(0)
-
-  const[score, setScore]=useState(0)
-
-  const present_question = Questions[questionIndex];
+   const[questionIndex, setQuestionIndex]= useState(0)
+   const[score, setScore]= useState(0)
+      const[results,setResults]=useState(false);
  
- 
- 
-  const get_choice =(index) =>
-{
-if(present_question.answer===index)
-{
- setScore=(score+1)
+const replay =() =>{
+   setQuestionIndex(0);
+   setScore(0);
+   setResults(false)
 }
-// const next_question = questionIndex+1;
-// if(next_question<Questions.length)
+   
+   const present_question = Questions[questionIndex];
+   const get_choice =(index) =>
+   {
+   if(present_question.answer==index)
+   {
+   setScore(score+1)
+   }
 
-// {
-// setQuestionIndex(questionIndex+1);
+   const next_question = questionIndex+1;
+   if(next_question<Questions.length)
 
+   {
+   setQuestionIndex(questionIndex+1);
+   }
+   else{
+      setResults(true);
+   }
 
-}  ;
+   }  ;
 
- 
+   
 
-    return (
-    <div className="quiz_box">
-       <div className="quiz_question">
-{present_question.question}
-<div className="options">Options below
-</div>
+   return (
+         <div className="quiz_box">
 
-<ul className="quiz_ul">
+{results?(<><h1>{score}</h1>
+<br/>
+<button  onClick= {replay}>Play Again</button>
+     
+       
+      </>
+      ):(
+         <div className="quiz_question">
+         {present_question.question}
+         
+         <div className="options"> 
+         </div>
+         <ul className="quiz_ul">
+         {present_question.options.map
+         (
+            (option,i)=> {
 
-  {present_question.options.map
-  (
-(option,i)=> {
+            return<li className='quiz_li' onClick={
+            () => get_choice(i)
 
-  return<li className='quiz_li' onClick={
-() => get_choice(i)
-    
-  }>{option}</li> 
-}
-  )
-  }
-</ul>
-       </div>
-    </div>
-  );
-}
+               }>{option}</li> 
+                        }
+         )
+      }
 
-export default App;
+         </ul>
+         </div>
+
+      )}
+              
+         </div>
+         );
+   }
+
+   export default App;
